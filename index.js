b356d12a67fae6cfa864e9a16f87d83d77e4509b
Julie Eggertsen <Julieeggertsen1@Christinas-MacBook-Pro.local>
2021-02-19 00:19:34 +0100

    Tilføjelse af class valgt, og href til singleview igennem info button

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 	const header = document.querySelector("header h1");
-	const medieurl = "https://rejsepanelet-2ad3.restdb.io/home/media";
+	const medieurl = "https://rejsepanelet-2ad3.restdb.io/home/media/";
 	const myHeaders = {
-		"x-apikey": "602e6aa25ad3610fb5bb6325"
+	    "x-apikey": "602e6aa25ad3610fb5bb6325"
 	}
 
 	document.addEventListener("DOMContentLoaded", start);
@@ -9,43 +9,53 @@
 	let filter = "alle";
 
 	function start() {
-		console.log("funktion start");
-		const filterKnapper = document.querySelectorAll("nav button");
-		filterKnapper.forEach(knap => knap.addEventListener("click", filtrerLande));
-		loadJSON();
+	    console.log("funktion start");
+	    const filterKnapper = document.querySelectorAll("nav button");
+	    filterKnapper.forEach(knap => knap.addEventListener("click", filtrerLande));
+	    loadJSON();
 	}
 
 	// funktion til filtrering
 	function filtrerLande() {
-		filter = this.dataset.kontinent;
-		visLande();
+	    //tilføjelse med class valgt
+	    filter = this.dataset.kontinent;
+	    document.querySelector(".valgt").classList.remove("valgt");
+	    this.classList.add("valgt");
+	    visLande();
+	    header.textContent = this.textContent;
 	}
 
 	async function loadJSON() {
-		const JSONData = await fetch("https://rejsepanelet-2ad3.restdb.io/rest/destinationer", {
-			headers: myHeaders
-		});
-		lande = await JSONData.json();
-		console.log("Lande", lande);
-		visLande();
+	    const JSONData = await fetch("https://rejsepanelet-2ad3.restdb.io/rest/destinationer", {
+	        headers: myHeaders
+	    });
+	    lande = await JSONData.json();
+	    console.log("Lande", lande);
+	    visLande();
 	}
 
 	// Vis lande i liste view
 	function visLande() {
-		console.log("Funktion visLande");
-		// container til lande og info fra json:
-		const dest = document.querySelector("#liste");
-		const skabelon = document.querySelector("template").content;
-		// Fjern al eksisterende tekst:
-		dest.textContent = "";
-		// loop igennem lande/json:
-		lande.forEach(land => {
-			if (filter == land.kontinent || filter == "alle") {
-				const klon = skabelon.cloneNode(true);
-				klon.querySelector(".h2_land").textContent = land.land;
-				klon.querySelector(".foto").src = medieurl + land.billede;
-				klon.querySelector(".kort_beskrivelse").textContent = land.kortBeskrivelse;
-				dest.appendChild(klon);
-			}
-		})
+	    console.log("Funktion visLande");
+	    // container til lande og info fra json:
+	    const dest = document.querySelector("#liste");
+	    const skabelon = document.querySelector("template").content;
+	    // Fjern al eksisterende tekst:
+	    dest.textContent = "";
+	    // loop igennem lande/json:
+	    lande.forEach(land => {
+	        if (filter == land.kontinent || filter == "alle") {
+	            const klon = skabelon.cloneNode(true);
+	            klon.querySelector(".h2_land").textContent = land.land;
+	            klon.querySelector(".foto").src = medieurl + land.billede;
+	            klon.querySelector(".kort_beskrivelse").textContent = land.kortBeskrivelse;
+	            //kommer ind på single view ved klik på info knap
+	            klon.querySelector(".info").addEventListener("click", () => visDetaljer(land));
+	            dest.appendChild(klon);
+	        }
+	    })
+	}
+
+	function visDetaljer(hvem) {
+	    location.href = `singleview.html?id=${hvem._id}`;
 	}
