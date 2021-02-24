4836be4d38b143855db0b3c3c8b50fe1715ae364
Emilie da Silva <emiliedasilva@Emilies-MacBook-Air-4.local>
2021-02-24 15:02:16 +0100

    kommentarer til burgerm menuen

--- script.js
+++ script.js
@@ -5,13 +5,16 @@ function sidenVises() {
 }
 
 //burgermenu
+//myFunction er det, der er skrevet ind i HTML koden i vores nav, på vores container, der indeholder de 3 barer, der udgør burgermenuen
 function myFunction(x) {
-    x.classList.toggle("change");
+    //ved tryk på burgermenuen føres man til myFunction(x), hvor man kan bruge toggle til at åbne og lukke burger menuen
+    x.classList.toggle("change"); //<-- change er CSS'en for barerne, her forsvinder bar 2, når der trykkes på burger menuen, så det bliver til et X
 
+    //her vælges der så, hvordan indholdet i myLinks skal opsættes, når der er trykket på burger menuen
     var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-        x.style.display = "none";
+    if (x.style.display === "block") { //<-- opsættes i block, når burger menuen er trykket på
+        x.style.display = "none"; //<-- vises ikke, når burger menuen er lukket
     } else {
-        x.style.display = "block";
+        x.style.display = "block"; //<-- opsættes i block igen
     }
 }
